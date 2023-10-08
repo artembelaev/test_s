1a6167ae9f5e3e1f00203b245a9c243205fae336
Artem Belyaev <artem.belyaev@playgendary.com>
2023-10-08 21:21:39 +0500

    - list node stub

---
+++ list_node.h
@@ -0,0 +1,35 @@
+#ifndef DEC_TO_BIN_H
+#define DEC_TO_BIN_H
+
+// структуру ListNode модифицировать нельзя
+struct ListNode 
+{
+	ListNode * prev = nullptr; // указатель на предыдущий элемент списка, либо `nullptr` в случае начала списка
+	ListNode * next = nullptr;
+	ListNode * rand = nullptr; // указатель на произвольный элемент данного списка, либо `nullptr`
+	std::string data; // произвольные пользовательские данные
+};
+
+class List 
+{
+public:
+
+	void Serialize(FILE * file) // сохранение списка в файл, файл открыт с помощью `fopen(path, "wb")`
+	{
+
+	}
+
+	void Deserialize(FILE * file) // восстановление списка из файла, файл открыт с помощью `fopen(path, "rb")`
+	{
+
+	}
+	
+	
+
+private:
+	ListNode * head = nullptr;
+	ListNode * tail = nullptr;
+	int count = 0;
+};
+
+#endif
--- tests/test_list_serialize.cpp
+++ tests/test_list_serialize.cpp
@@ -1,6 +1,8 @@
 #include <boost/test/unit_test.hpp>
+#include <list_node.h>
 
-BOOST_AUTO_TEST_CASE(list_serialize) {
+BOOST_AUTO_TEST_CASE(list_serialize) 
+{
     // Your test code here
     BOOST_CHECK(true);
 }
